aa0b3c0e796c46cbe7cd0a670774ff835d277f8f
Marcus Antonius <marcusantonius88@gmail.com>
2022-11-24 08:31:13 -0300

    Ajuste na quebra de linha

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 
 function exibeMensagem(array $tabuas){
-    echo 'Tabua das mares JP de hoje: ' . date('d/m/Y') .  PHP_EOL;
+    echo 'Tabua das mares JP de hoje: ' . date('d/m/Y') .  '<br/>';
     foreach($tabuas as $tabua){
-        echo 'Hora: '. str_pad($tabua['hora'], '5', '0', STR_PAD_LEFT)  . ' | Altura: ' .str_replace(' ','', $tabua['altura']) . PHP_EOL;
+        echo 'Hora: '. str_pad($tabua['hora'], '5', '0', STR_PAD_LEFT)  . ' | Altura: ' .str_replace(' ','', $tabua['altura']) . '<br/>';
     }
 }
